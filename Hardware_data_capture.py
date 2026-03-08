9489e0cd05f74462eece31e243c5b5db4320774b
murilo2305 <murilo.santos.silva.v@gmail.com>
2026-03-08 15:45:16 -0300

    Scipts de escrita e leitura de csv

---
+++ Hardware_data_capture.py
@@ -0,0 +1,58 @@
+import psutil
+import time
+import csv
+from datetime import datetime
+import os
+
+data = ["USER","DATETIME","CPU","RAM","MEM","TEMP","BYSEND1","BYSEND2","BYRECV1","BYRECV2"]
+
+arquivoCSV = 'Hardware_data/Hardware_data.csv'
+
+if os.path.exists(arquivoCSV) == False:
+
+    with open(arquivoCSV, mode = "w", newline='') as arq:
+
+            escritor = csv.writer(arq)
+            escritor.writerow(data)
+
+while True:
+
+    mem = (psutil.disk_usage('/'))
+    ram = psutil.virtual_memory()
+    cpu = psutil.cpu_percent(interval=1, percpu=False)
+    date = datetime.now()
+    usuario = os.getlogin()
+    curTemp = "SysUnsurp"
+    netSpd = psutil.net_io_counters()
+
+    try:
+         
+        temp = psutil.sensors_temperatures()
+        curTemp = ["coretemp"][0][1]
+
+        print(temp)
+
+    except AttributeError:
+
+        print("Captura de temperatura não é superdada pelo seu sistema operacional")         
+
+    bytsSendSpd1 = netSpd.bytes_sent
+    bytsRecvSpd1 = netSpd.bytes_recv
+    
+    time.sleep(5)
+
+    netSpd = psutil.net_io_counters()
+
+    bytesSendSpd2 = netSpd.bytes_sent
+    bytesRecvSpd2 = netSpd.bytes_recv
+
+
+
+    data = [usuario,date.strftime("%Y-%m-%d %H:%M:%S"),cpu,round(ram.used/(1024**3),2),mem.percent,curTemp,bytsSendSpd1,bytesSendSpd2,bytsRecvSpd1,bytesRecvSpd2]
+
+    with open(arquivoCSV, mode = "a", newline='') as arq:
+
+        escritor = csv.writer(arq)
+        escritor.writerow(data)
+
+    time.sleep(4)
---
+++ Hardware_data_reader.py
@@ -0,0 +1,159 @@
+import csv
+from datetime import datetime
+import os
+import psutil
+import time
+from itertools import islice
+
+arquivoCSV = 'Hardware_data/Hardware_data.csv'
+formatoData = "%Y-%m-%d %H:%M:%S"
+linhas = 0
+ramTotal = round(8367104000/1024**3,2)
+picoRam10min = []
+picoMem10min = []
+picoCPU10min = []
+PicoSendSpd = []
+PicoRecvSpd = []
+PicoTemp = []
+dtTimes = []
+count = 0
+user = ''
+
+def Leitura():
+
+    with open (arquivoCSV,'r',newline="") as arq:
+
+        leitor = csv.reader(arq)
+
+        linhas = sum(1 for row in leitor)
+
+    with open (arquivoCSV,"r") as arq:
+
+        leitor = csv.reader(arq)
+
+        return next(islice(leitor,linhas-1,linhas))
+
+def timeFrame10s():
+
+    count = 0
+
+    if os.path.exists(arquivoCSV):
+
+        while True:
+
+            resultado = Leitura()
+
+            user = resultado[0]
+            Ram = float(resultado[3])
+            Mem = float(resultado[4])
+            Cpu = float(resultado[2])
+            Date =  datetime.strptime(resultado[1],formatoData)
+            vSend = float(round(((float(resultado[7])-float(resultado[6])/5))/1000000,2))
+            vRecv = float(round(((float(resultado[9])-float(resultado[8])/5))/1000000,2))
+            temp = resultado[5]
+
+            print("Ram: ",round((Ram/ramTotal)*100,2),"%","\n","Memória: ",Mem,"%","\n","CPU: ",Cpu,"%","\n","Data: ",Date,"\n")
+
+            if(len(PicoSendSpd) == 60):
+
+                PicoSendSpd.pop(0)
+
+            PicoSendSpd.append(vSend)
+
+            if(len(PicoRecvSpd) == 60):
+
+                PicoRecvSpd.pop(0)
+
+            PicoRecvSpd.append(vRecv)
+
+            if temp != "SysUnsurp":
+
+                if(len(PicoTemp) == 60):
+
+                    PicoTemp.pop(0)
+
+                PicoTemp.append(temp)
+
+            PicoRecvSpd.append(vRecv)
+
+            if len(dtTimes) == 60:
+
+                dtTimes.pop(0)
+            
+            dtTimes.append(Date)
+
+            count = count + 1            
+
+            if len(picoRam10min) == 60:
+
+                picoRam10min.pop(0)
+            
+            picoRam10min.append(Ram)
+
+            if len(picoCPU10min) == 60:
+
+                 picoCPU10min.pop(0)
+            
+            picoCPU10min.append(Cpu)
+
+            if len(picoMem10min) == 60:
+
+                picoMem10min.pop(0)
+            
+            picoMem10min.append(Mem)
+
+            print("Pico do uso de Ram no últimos 10 minutos: ",round((max(picoRam10min)/ramTotal)*100,2),"%\n",
+                         "Pico do uso de CPU nos últimos 10 minutos: ",max(picoCPU10min),"%\n",
+                         "Pico do uso de memória nos últimos 10 minutos: ",max(picoMem10min),"%\n")
+
+            if count == 60:
+
+                data = ["usuario","dtIni","dfFin","PicoCPU","PicoRam","PicoMem","MediaCPU","MediaRam","MediaMem","PicoVelEnvio","MedVelEnvio","PicoVelReceb","MediaVelReceb","PicoTemp","MediaTemp"]
+
+                CSV = 'Hardware_data/Treated_ardware_data.csv'
+
+                if os.path.exists(CSV) == False:
+
+                    with open(CSV, mode = "w", newline='') as arq:
+
+                            escritor = csv.writer(arq)
+                            escritor.writerow(data)
+
+
+                Pmem = f"{(max(picoMem10min))}%"
+                Pram = f"{round((max(picoRam10min)/ramTotal)*100,2)}%"
+                Pcpu = f"{max(picoCPU10min)}%"
+                Mmem = f"{sum(picoMem10min)/len(picoMem10min)}%"
+                Mram = f"{round(((sum(picoRam10min)/(ramTotal*len(picoRam10min)))/len(picoRam10min))*100,2)}%"
+                Mcpu = f"{round(sum(picoCPU10min)/len(picoCPU10min),2)}%"
+                PVSend = f"{round(max(PicoSendSpd),2)}Mbps"
+                PVRecv = f"{round(max(PicoRecvSpd),2)}Mbps"
+                MVSend = f"{round(sum(PicoSendSpd)/len(PicoSendSpd),2)}Mbps"
+                MVRecv = f"{round(sum(PicoRecvSpd)/len(PicoRecvSpd),2)}Mbps"
+                dtIni = dtTimes[0]
+                dtFin = dtTimes[len(dtTimes)-1]
+
+                if len(PicoTemp) == 0:
+
+                    PTemp = "SysUnsurpC"
+                    MTemp = "SysUnsurpC"
+
+                else:
+
+                    PTemp = f"{max(PicoTemp)}C"
+                    MTemp = f"{sum(PicoTemp)/len(PicoTemp)}C"
+
+                data = [user,dtIni,dtFin,Pcpu,Pram,Pmem,Mcpu,Mram,Mmem,PVSend,MVSend,PVRecv,MVRecv,PTemp,MTemp]
+
+                with open(CSV, mode = "a", newline='') as arq:
+
+                    escritor = csv.writer(arq)
+                    escritor.writerow(data)
+
+                count = 0
+
+    else:
+    
+        print('Erro: o csv não existe')
+
+timeFrame10s()

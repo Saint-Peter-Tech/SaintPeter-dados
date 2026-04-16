1765d9feb8b6ee2a2ad7f446e4309c39c6c60989
Philipi <philipi.rodrigues@sptech.school>
2026-04-16 20:14:35 -0300

    Script de envio à S3 integrado ao de captura.

--- python/raw.py
+++ python/raw.py
@@ -8,6 +8,9 @@ import subprocess
 import random
 from random import randint
 import sys
+from botocore.exceptions import ClientError
+import os
+import logging
 
 # Importando Bibliotecas Necessárias:
 # psutil = Captura de Hardware e processos;
@@ -256,6 +259,31 @@ def verificar_modulos():
 
     return status_modulos
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3',
+        aws_access_key_id="",
+        aws_secret_access_key="",
+        aws_session_token="")
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 try:
     while True:
 
@@ -353,6 +381,9 @@ try:
 
             time.sleep(60)
             # Aguarda mais 60 segundos antes da próxima coleta (controle de frequência).
+
+            upload_file("../dados_brutos/dados_brutos.csv", "saint-peter", "raw/dados_brutos.csv")
+
 except KeyboardInterrupt:
     print("Encerrando Monitoramento...")
     
